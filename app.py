04d9837feea4aad766db87420bb357e340802200
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-04-04 15:55:23 +0000

    created login modal/popup

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import config
@@ -10,9 +10,12 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"]=config.CONFIG['MONGO_DBNAME']
 app.config["MONGO_URI"]=config.CONFIG['MONGO_URI']
+app.config["SECRET_KEY"]=config.CONFIG['SECRET_KEY']
 
 mongo = PyMongo(app)
 
+user_information=mongo.db.user_information
+recipe_collection=mongo.db.recipes
 
 @app.route('/')
 def index():
@@ -21,7 +24,24 @@ def index():
 @app.route('/recipes')
 def recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
+@app.route('/login', methods=['POST'])
+def login():
+   if request.method == "POST":
+        username = request.form['username']
 
+        try:
+            user_in_db = user_information.find_one({"username": username})
+        except:
+            flash("Sorry there seems to be problem with the data")
+            return redirect(url_for('index'))
+        if user_in_db:
+            flash("Logged in as {username}")
+            return_url = request.referrer
+            return redirect(return_url)
+        else:
+            flash("Sorry no profile {request.form['username']} found")
+            return redirect(url_for('index'))
 
 
 
