cf86ba1d938564d0dcdbd24d834617748a5ac33f
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-04-16 12:16:50 +0000

    completed signup and login functionality

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 import config
 from datetime import date, datetime
 from pprint import pprint
+from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
@@ -53,7 +54,54 @@ def recipe(recipe_id):
     pprint(recipe)
     return render_template('recipe.html', recipe=a_recipe)
 
-
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if 'logged_in' in 'session':
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm() 
+    if form.validate_on_submit():
+            
+        user = mongo.db.user_information
+        dup_user = user.find_one({'name' : request.form['username'].title()})
+            
+        if dup_user is None:
+            user.insert_one({'name' : request.form['username'].title()})
+            session['username'] = request.form['username']
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        
+        flash('Sorry, username already taken. Please try another.')
+        return redirect(url_for('signup'))
+                
+    return render_template('signup.html', form=form, title="Sign Up")
+    
+@app.route('/login', methods=['GET', 'POST'])
+def user_login():
+    """Function for handling the logging in of users"""
+    if 'logged_in' in session: #Check is already logged in
+        return redirect(url_for('index'))
+    
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        user = mongo.db.user_information
+        logged_in_user = user.find_one({'name' : request.form['username'].title()})
+        
+        if logged_in_user is None:
+            flash('Incorrect username, please try again')
+            return redirect(url_for('user_login'))
+        session['username'] = request.form['username']
+        session['logged_in'] = True
+        return redirect(url_for('index'))
+        
+    return render_template('login.html', form=form, title='Login')
+    
+@app.route('/logout')
+def logout():
+    """Logs the user out and redirects to home"""
+    session.clear() # Kill session
+    return redirect(url_for('index'))
     
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, Optional
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
