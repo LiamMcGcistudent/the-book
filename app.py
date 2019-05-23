cea49f60e611bf5e8f0e4ba2dbd8d42fed948cb7
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-05-23 15:48:02 +0000

    updated Jinja version in requirements.txt; updated config; removed datetime from app.py due to non-use

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os, math
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
-from datetime import date, datetime
 from pprint import pprint
 from forms import RegistrationForm, LoginForm
 import logging
